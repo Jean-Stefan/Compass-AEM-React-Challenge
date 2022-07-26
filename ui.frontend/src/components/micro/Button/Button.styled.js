0d269a7d39a2d9374cf71f5dd66a5f130ad2da98
Jean-Stefan <jeanss12@gmail.com>
2022-07-26 16:43:53 -0300

    Feat: added button react code

---
+++ ui.frontend/src/components/micro/Button/Button.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    width: 216px;
+    height: 68px;
+    background: #333333;
+    color: #ffffff;
+    font-size: 14px;
+    font-weight: 700;
+    font-family: 'Space Mono', monospace;
+    letter-spacing: -0.035em;
+    text-transform: uppercase;
+`;
+
+export default StyledButton;
